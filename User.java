db5ce6d495b98bb5ddc7f210c0fb74ecb624954b
Mehgan Cook <mehgancook@yahoo.com>
2015-11-21 02:46:38 -0800

    updated user

--- User.java
+++ User.java
@@ -1,4 +1,62 @@
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class User {
+	String myFirstName;
+	String myLastName;
+	String mySchool;
+	Image myImage;
+	List<String> myClasses;
+	List<User> myFriends;
+	
+	
+	
+	public User(String theFirstName, String theLastName, String theSchool, Image theImage,
+				List<String> theClasses) {
+		myFirstName = theFirstName;
+		myLastName = theLastName;
+		mySchool = theSchool;
+		myImage = theImage;
+		myClasses = new ArrayList<String>(theClasses);
+	}
+	
+	public List<String> getClasses() {
+		return myClasses;
+	}
+	 
+	public void setFirstName(String theFN) {
+		myFirstName = theFN;
+	}
+	
+	public void setLastname(String theLN) {
+		myLastName = theLN;
+	}
+	
+	public void setSchool(String school) {
+		mySchool = school;
+	}
+	
+	public String getSchool() {
+		return mySchool;
+	}
+	
+	public List<User> getFriends() {
+		return myFriends;
+	}
+	
+	public void addFriend(User friend) {
+		myFriends.add(friend);
+	}
+	
+	public void removeFriend(User friend) {
+		if (myFriends.contains(friend)) {
+			int i = myFriends.indexOf(friend);
+			myFriends.remove(i);
+		} else {
+			System.out.println("Friend already Removed!");
+		}
+	}
 
 }
