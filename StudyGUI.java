82b1ed3078faf7e74e76ef0592eca82c71a15c0c
https://github.com/StudyBuddy-App/Website <shmurphy@uw.edu>
2015-11-21 02:51:21 -0800

    gui commit

--- StudyGUI.java
+++ StudyGUI.java
@@ -1,5 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,6 +8,7 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -21,16 +23,27 @@ public class StudyGUI {
 	private final JFrame myFrame;
 	private static final Toolkit KIT = Toolkit.getDefaultToolkit();
 
-	private static final Dimension SCREEN_SIZE = KIT.getScreenSize();
+	
+//	private static final Dimension SCREEN_SIZE = KIT.getScreenSize();
 
 	
     public StudyGUI() {
     	myFrame = new JFrame("Study Buddies");
     	myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myFrame.setLocation(SCREEN_SIZE.width / 2 - myFrame.getWidth() / 2,
-                SCREEN_SIZE.height / 2 - myFrame.getHeight() / 2);
+//        myFrame.setLocation(SCREEN_SIZE.width / 2 - myFrame.getWidth() / 2,
+//                SCREEN_SIZE.height / 2 - myFrame.getHeight() / 2);
         myFrame.setResizable(true);
         myFrame.setVisible(true);
+        
+        JPanel homePanel = new JPanel(new GridLayout());
+        homePanel.setVisible(true);
+        homePanel.setPreferredSize(new Dimension(1, 500));
+        myFrame.add(homePanel, BorderLayout.CENTER);
+        
+        JOptionPane login = new JOptionPane();
+        login.showConfirmDialog(homePanel, "Login");
+        myFrame.add(login);
+        
         myFrame.pack();
     }
 }
